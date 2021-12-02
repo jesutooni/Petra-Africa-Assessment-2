08ce965c48c5feca2a3b27f38b173fa41dcc01a3
Jesutooni <edeworuyoyou@gmail.com>
2021-12-02 16:41:29 +0100

    navbar

---
+++ src/components/Nav.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <div class="flex justify-between mb-4">
+      <h1 class="text-4xl text-blueDark font-bold">Welcome, Petra Africa</h1>
+      <button
+        @click="signOut"
+        class="text-white font-bold bg-blueDark w-28 py-2 rounded-md"
+      >
+        Sign Out
+      </button>
+    </div>
+    <hr />
+  </div>
+</template>
+
+<script>
+export default {
+  methods: {
+    signOut() {
+      this.$router.push("/");
+    },
+  },
+};
+</script>
