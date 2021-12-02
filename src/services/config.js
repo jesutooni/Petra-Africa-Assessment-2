1b4e427f4acd703d5a4e9a2595ee81bc3fd90e42
Jesutooni <edeworuyoyou@gmail.com>
2021-12-02 16:47:37 +0100

    fetch transactions

--- src/services/config.js
+++ src/services/config.js
@@ -4,6 +4,9 @@ class ContributionServices {
   getEmployees() {
     return http.get("/customer");
   }
+  getTransactions() {
+    return http.get("/transaction");
+  }
 }
 
 export default new ContributionServices();
